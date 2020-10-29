5456b7e9676e92ad8e7ae5a91890e7c4d8a8d568
Ann <zubenko.job@gmail.com>
2020-10-29 10:21:47 +0200

    adaptability

--- index.php
+++ index.php
@@ -8,69 +8,81 @@ include "configs/bd.php";
 <head>
 	<title>Cat_chat</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+	
 </head>
+
 <body>
 	<?php 
+	//подключаем шапку сайта
 	include "parts/header.php";
-	if (!isset($_COOKIE["user_id"])) { ?>
-	<div class='parent'>
-		<div class="hello"></div>
-	</div>
-	<?php include "parts/modalLogin.php";
-	include "parts/modalRegistration.php";
-	} else { ?>
+	//если пользователь не аворизован(не существуют куки "user_id")
+	if (!isset($_COOKIE["user_id"])) { 
+	//выводим окно приветсвия
+	?>
+		<div class='parent'>
+			<div class="hello"></div>
+		</div>
+	<?php 
+		//подключаем модальное окно для авторизации
+		include "parts/modalLogin.php";
+		//и регистрации
+		include "parts/modalRegistration.php";
+	//если авторизован 
+	} else { 
+	//выводим основной контент
+	?>
+		<div class="content">
+			<div class="user">
+				<form action="/parts/userSearch.php" metod="POST" class="search">
+					<input type="text" name="poisk-text">
+					<button id="searchContact">
+						<img src="/css/images/search.png">
+					</button>
+				</form>
 
-	<div class="content">
-		<div class="user">
-			<form action="/parts/userSearch.php" metod="POST" class="search">
-				<input type="text" name="poisk-text">
-				<button id="searchContact">
-					<img src="/css/images/search.png">
-				</button>
-			</form>
+				<div class="list">			
+					<ul>
+					<?php
+					//подключаем список пользователей
+					include "parts/listUsers.php";
+					?>
+					</ul>
+				</div>
+			</div>
 
-			<div class="list">			
-				<ul>
-				<?php
-				//Список пользователей
-				include "parts/listUsers.php";
+			<div class="message">
+				<?php //если существует выбранный пользователь(отправленый через GET-запрос)
+				//выводим блок с перепиской и подключаем скрипт с ajax-запросами для отправки сообщений
+				if (isset($_GET["user"])) {
+					//подключаем шапку блока с перепиской
+					include "parts/headerFriend.php"; ?>
+					<div class="sms">
+						<?php //подключаем вывод сообщений
+						include "parts/listMessage.php"; 
+						?>
+					</div>
+
+					<form id="form" action="/modules/setMessage.php" method="POST">
+						<input type="hidden" name="id_user_2" value="<?php echo $_GET["user"]; ?>" >
+						<input type="hidden" name="id_user" value="<?php echo $_COOKIE["user_id"]; ?>" >
+						<textarea name="text"></textarea>
+						<button type="submit" id="send_sms">
+							<img src="/css/images/sent.png">
+						</button>
+					</form>
+					<script src='js/ajaxSMS.js'></script>
+				<?php //если нет то не выводим ничего
+				} else { }
 				?>
-				</ul>
 			</div>
 		</div>
 
-		<div class="message">
-
-			
-				<?php
-					if (isset($_GET["user"])) {
-						//$user_friend = $_GET["user"];
-					include "parts/headerFriend.php"; ?>
-					<div class="sms">
-					<?php include "parts/listMessage.php"; ?>
-					</div>
-				<form id="form" action="/modules/setMessage.php" method="POST">
-				
-				<input type="hidden" name="id_user_2" value="<?php echo $_GET["user"]; ?>" >
-				<input type="hidden" name="id_user" value="<?php echo $_COOKIE["user_id"]; ?>" >
-				<textarea name="text"></textarea>
-				<button type="submit" id="send_sms">
-					<img src="/css/images/sent.png">
-				</button>
-			</form>
-			<script src='js/ajaxSMS.js'></script>
-				<?php
-					} else { }
-				?>
-		</div>
-	</div>
-	<script src='js/searchUser.js'></script>
-	<script src='js/online.js'></script>
+		<script src='js/searchUser.js'></script>
+		<script src='js/online.js'></script>
 	<?php
-	//подключение модального окна с друзьями
-	include 'parts/modalFriends.php'; 
+		//подключаем модальное окна с друзьями
+		include 'parts/modalFriends.php'; 
 	}
 	?>
-	
 </body>
 </html>
--- modules/addFriends.php
+++ modules/addFriends.php
@@ -2,12 +2,11 @@
 //подключаем файл с подключением к БД
 include $_SERVER['DOCUMENT_ROOT'] . "/configs/bd.php";
 
-if(isset($_GET["user"])){
-	$sql = "INSERT INTO friends (user_1, user_2) VALUE ('" . $_COOKIE["user_id"] . "', '" . $_GET["user"] . "')";
-	if(mysqli_query($connect, $sql)) {
-		header ('Location:/index.php?user=' . $_GET["user"]);
-	} else {
-		echo "<h2>Произошла ошибка</h2>";
-	}
+//
+$sql = "INSERT INTO friends (user_1, user_2) VALUE ('" . $_COOKIE["user_id"] . "', '" . $_GET["user"] . "')";
+if(mysqli_query($connect, $sql)) {
+	header ('Location:/index.php?user=' . $_GET["user"]);
+} else {
+	echo "<h2>Произошла ошибка</h2>";
 }
 ?>
